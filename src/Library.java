17f2ad7bea2d5e68d4ea31047a712649d2789e98
Andrea Kaus <andrea.kaus.94@gmail.com>
2020-02-20 19:29:02 +0100

    *jhhjhj

--- src/Library.java
+++ src/Library.java
@@ -11,6 +11,16 @@ public class Library {
 		this.qBooksPerDay = qBooksPerDay;
 		books = new Book[qBooks];
 	}
+	public int calcBookWeight(){
+		int score = 0;
+		if(books == null){
+			return 0;
+		}
+		for(int i = 0; i < books.length; i++){
+			score += books[i].value;
+		}
+		return score;
+	}
 	
 	public void addBook(Book b) {
 		books[b.idx] = b;
