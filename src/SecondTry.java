96424fc6e4b16ec4ef83cdc6ef9b2086bbc82a4a
danilo <dfink91@gmail.com>
2020-02-20 20:33:17 +0100

    no message

--- src/SecondTry.java
+++ src/SecondTry.java
@@ -11,7 +11,7 @@ public class SecondTry {
 			System.out.println("time remaining: " + days);
 			if (days > 0 && l.qBooks > 0) {
 				sendingLibs.add(l);
-				int maxBooks = days * l.qBooksPerDay;
+				long maxBooks = days * l.qBooksPerDay;
 				for (int iB = 0; iB < maxBooks && iB < l.qBooks; iB++) {
 					l.sendBook(l.books.get(iB));					
 				}
--- src/firstTry.java
+++ src/firstTry.java
@@ -1,17 +1,20 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class firstTry {
-	public static ArrayList<Library> calcLibraries(Book[] books, Library[] libs, int days) {
+	public static ArrayList<Library> calcLibraries(Book[] books, Library[] libs, long days) {
 		ArrayList<Library> sendingLibs = new ArrayList<>();
 		for(int iL = 0, cntL = libs.length; iL < cntL && days >= 0; iL++) {
 			Library l = libs[iL];
-			days -= l.signUpTime;
-			if (days > 0 && l.qBooks > 0) {
-				sendingLibs.add(l);
-				l.books.sort((o1, o2) -> o1.value - o2.value);
-				int maxBooks = days * l.qBooksPerDay;
-				for (int iB = 0; iB < maxBooks && iB < l.qBooks; iB++) {
-					l.sendBook(l.books.get(iB));					
+			if (l.qBooks > 0) {
+				days -= l.signUpTime;
+				if (days > 0) {
+					sendingLibs.add(l);
+					l.books.sort((o1, o2) -> o1.value - o2.value);
+					BigInteger maxBooks = BigInteger.valueOf(days).multiply(BigInteger.valueOf(l.qBooksPerDay));
+					for (long iB = 0; iB < maxBooks.longValue() && iB < l.qBooks; iB++) {
+						l.sendBook(l.books.get((int)iB));					
+					}
 				}
 			}
 		}
